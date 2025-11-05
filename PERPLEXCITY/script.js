def838716b161f94ffff29a621b1084c35d803fd
Ai777-Future <aifuture7777@gmail.com>
2025-11-05 14:00:13 +0530

    Delete PERPLEXCITY directory

--- PERPLEXCITY/script.js
+++
@@ -1,7 +0,0 @@
-// script.js
-window.addEventListener('DOMContentLoaded', () => {
-  const video = document.querySelector('.gym-video');
-  if (video) {
-    video.removeAttribute('controls');
-  }
-});
